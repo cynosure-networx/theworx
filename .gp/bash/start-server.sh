627fd5974d186333029e2769b492305b634b6ab8
Paul Willridge <paul@cynosurenetworx.com>
2022-06-28 18:02:31 +0000

    changed from depricated await-port

--- .gp/bash/start-server.sh
+++ .gp/bash/start-server.sh
@@ -16,10 +16,10 @@ fi;
 UP=$(pgrep mysql | wc -l)
 if [[ $UP -ne 1 ]]; then
   start_spinner "Initializing MySql..." &&
-  gp await-port 3306 &&
+  gp ports await 3306 &&
   stop_spinner $?
 fi 
-gp await-port 3306 &&
+gp ports await 3306 &&
 __port=$(bash .gp/bash/helpers.sh get_default_server_port)
 __server=$(bash .gp/bash/utils.sh parse_ini_value starter.ini development default_server)
 start_spinner "Starting $__server server on port $__port when system is ready..." &&
